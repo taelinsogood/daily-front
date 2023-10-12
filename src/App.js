a0c7f67ad80e292c3b2a8396d88476ecb6f80f76
taelinsogood <tyui0330op@gmail.com>
2023-10-12 14:20:37 +0900

    메모

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import AddEmployee from './components/AddEmployee';
+import Navbar from './components/Navbar';
+import EmployeeList from './components/EmployeeList';
+import UpdateEmployee from './components/UpdateEmployee';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<EmployeeList/>}/>
+        <Route path='/addEmployee' element={<AddEmployee/>}/>
+        <Route path='/editEmployee/:id' element={<UpdateEmployee/>}/>
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
